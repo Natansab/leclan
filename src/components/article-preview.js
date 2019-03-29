ae6f8505e31ed67fba02919f2dcd9c5623d63643
Natan Sabbah <nsabbah@student.42.fr>
2019-03-29 10:44:42 +0300

    fixed error & disabled fadeIn

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -5,21 +5,23 @@ import Img from 'gatsby-image'
 import styles from './article-preview.module.css'
 
 export default ({ article }) => (
-  <div class="card-article">
+  <div className="card-article">
     <Link to={`/blog/${article.slug}`}>
-      <Img alt={article.title} fluid={article.blogPostPreviewImage.fluid} />
+      <Img
+        fadeIn={false}
+        alt={article.title}
+        fluid={article.blogPostPreviewImage.fluid}
+      />
     </Link>
-    <div class="card-article-desc">
+    <div className="card-article-desc">
       <Link to={`/blog/${article.slug}`}>
         <h2>{article.title}</h2>
       </Link>
-      <p>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: article.description.childMarkdownRemark.html,
-          }}
-        />
-      </p>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: article.description.childMarkdownRemark.html,
+        }}
+      />
     </div>
   </div>
 )
