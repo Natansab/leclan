31c96d8b5869f0dc8f036ccd539a347841843c42
Natan Sabbah <nsabbah@student.42.fr>
2019-03-31 23:57:11 +0300

    manage css properly

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -5,15 +5,19 @@ import Img from 'gatsby-image'
 import styles from './article-preview.module.css'
 
 export default ({ article }) => (
-  <div className="card-article">
+  <div className={styles.card_article}>
     <Link to={`/blog/${article.slug}`}>
-      <Img
+      <img
+        src={article.blogPostPreviewImage.fluid.src}
+      />
+      {/* <Img
+        // as='img'
         fadeIn={false}
         alt={article.title}
         fluid={article.blogPostPreviewImage.fluid}
-      />
+      /> */}
     </Link>
-    <div className="card-article-desc">
+    <div className={styles.card_article_desc}>
       <Link to={`/blog/${article.slug}`}>
         <h2>{article.title}</h2>
       </Link>
